fd2bf9fb23951fc170edb818b3ff17bd94c98744
Angel Trevino <angel.trevinov@gmail.com>
2021-06-01 16:31:21 -0500

    comenting types and semantic cube

--- GenerateCode/Types.py
+++ GenerateCode/Types.py
@@ -2,7 +2,15 @@ import enum
 
 
 class Types(enum.Enum):
-    """To define the different variable types"""
+    """
+    To define the different variable types possible in the language.
+
+    :Date: 06-02-2021
+    :Version: 1
+    :Authors:
+        - Angel Treviño A01336559
+        - Julia Jimenez A00821428
+    """
     INVALID = "null"
     INT     = "int"
     FLOAT   = "float"
