9e8703ad65fcc74cbdc138c6e07f7c49400cf2d7
Angel Trevino <angel.trevinov@gmail.com>
2021-06-02 17:48:31 -0500

    Adding comments for vars dir

--- GenerateCode/Variables_Dir.py
+++ GenerateCode/Variables_Dir.py
@@ -1,5 +1,15 @@
 
 class Variables_Dir:
+    """
+    Class that defines the Variables Directory used inside
+    a class or inside a function.
+
+    :Date: 06-02-2021
+    :Version: 1
+    :Authors:
+        - Angel Treviño A01336559
+        - Julia Jimenez A00821428
+    """
 
     def __init__(self):
         self.__var_dict__ = {}  # the var directory object
@@ -15,12 +25,19 @@ class Variables_Dir:
         return self.__var_dict__[name_var]
 
     def add_to_dictionary(self, name_var: str, type: str, cont: int, dims: [] = None, R: int = 0):
-        """ Adds the incoming variable with its type to the variable directory.
+        """ Adds the incoming variable with its type, address,
+        dimensions and R to the variable directory.
 
         :param name_var: Name of the variable
         :type name_var: str
         :param type: The type of the variable
         :type type: str
+        :param cont: The address of the variable
+        :type type: int
+        :param dims: The limits for each dimension, if its not an array its none
+        :type type: list
+        :param R: The spaces the array occupies, if its not an array its 0
+        :type type: int
         """
         self.__var_dict__[name_var] = {
             "type": type,
